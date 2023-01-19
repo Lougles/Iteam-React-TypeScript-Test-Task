25ec089be32b520674acb13c5f8928f7071294f8
vovacelidze <louglescom@gmail.com>
2023-01-19 22:44:20 +0200

    added logo and input in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,14 @@
 import React, { FC } from 'react'
 import style from'./Header.module.scss'
-
+import steamLogo from '../../shared/images/logo/steam-logo.png'
+import searchLogo from '../../shared/images/logo/input-logo.png'
 
 const Header: FC = () => {
   return (
     <div className={style.Header_Wrapper}>
-      <div className="div"></div>
-      <div className="div"></div>
-      <div className="div"></div>
-      <div className="div"></div>
-      <div className="div"></div>
+      <img className={style.logo} src={steamLogo} />
+      <input placeholder='Enter an app name...' className={style.Header_Input}></input>
+      
     </div>
   )
 }
