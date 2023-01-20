02dc4edf1e51785e5a260615311622b61e0e001a
vovacelidze <louglescom@gmail.com>
2023-01-20 18:22:08 +0200

    added dropdown btn with two options

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,23 @@
 import React, { FC } from 'react'
 import style from'./Header.module.scss'
 import steamLogo from '../../shared/images/logo/steam-logo.png'
-import searchLogo from '../../shared/images/logo/input-logo.png'
+import checkboxloggo from '../../shared/images/logo/pricelowbigbtn.png'
+
 
 const Header: FC = () => {
   return (
     <div className={style.Header_Wrapper}>
       <img className={style.logo} src={steamLogo} />
       <input placeholder='Enter an app name...' className={style.Header_Input}></input>
-      
+      <div className={style.dropdown}>
+        <button className={style.Profile_Btn}>
+        <img src={checkboxloggo} />
+        </button>
+        <div className={style.dropdownOptions}>
+          <p className={style.link} >Lower to bigger</p>
+          <p className={style.link} >Bigger to lower</p>
+        </div>
+      </div>
     </div>
   )
 }
