e4722ec6c208006cf55a635d6e848ce4abbb9ce3
vovacelidze <louglescom@gmail.com>
2023-01-21 15:39:08 +0200

    added open/close btn, and thinking to do the same select

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,42 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import style from'./Header.module.scss'
 import steamLogo from '../../shared/images/logo/steam-logo.png'
 import checkboxloggo from '../../shared/images/logo/pricelowbigbtn.png'
+import Select from 'react-select'
+import { colourOptions } from './data';
+
 
 
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen(true)
+  const hanleClose = () => setIsOpen(false)
+
+
   return (
     <div className={style.Header_Wrapper}>
       <img className={style.logo} src={steamLogo} />
       <input placeholder='Enter an app name...' className={style.Header_Input}></input>
       <div className={style.dropdown}>
-        <button className={style.Profile_Btn}>
+        <button className={style.Profile_Btn} onClick={handleOpen}>
         <img src={checkboxloggo} />
         </button>
+        {
+          isOpen &&
         <div className={style.dropdownOptions}>
-          <p className={style.link} >Lower to bigger</p>
-          <p className={style.link} >Bigger to lower</p>
+          <p onClick={hanleClose} className={style.link}>Lower to bigger</p>
+          <p onClick={hanleClose} className={style.link} >Bigger to lower</p>
         </div>
+        }
       </div>
+      <Select
+        className={style.select}
+        classNamePrefix="select"
+        defaultValue={colourOptions[0]}
+        name="color"
+        options={colourOptions}
+      />
     </div>
   )
 }
